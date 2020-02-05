d686f0040b0f34dc5849c0847300a40e46ab7159
Johnny Peixoto <johnny.peixotto@outlook.com>
2020-02-05 20:51:30 -0300

    first commit

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://d26lpennugtm8s.cloudfront.net/stores/726/919/products/camiseta-preta-st-center-ec76e86b0df341498315420426066924-240-0.png"
+                alt="Camiseta"
+              />
+            </td>
+            <td>
+              <strong>Camiseta Geek</strong>
+              <span>R$ 80,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 161,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 280,90</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://d26lpennugtm8s.cloudfront.net/stores/726/919/products/camiseta-preta-st-center-ec76e86b0df341498315420426066924-240-0.png"
+          alt="Camiseta"
+        />
+        <strong>Camiseta Geek</strong>
+        <span>R$ 80,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://d26lpennugtm8s.cloudfront.net/stores/726/919/products/camiseta-preta-st-center-ec76e86b0df341498315420426066924-240-0.png"
+          alt="Camiseta"
+        />
+        <strong>Camiseta Geek</strong>
+        <span>R$ 80,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://d26lpennugtm8s.cloudfront.net/stores/726/919/products/camiseta-preta-st-center-ec76e86b0df341498315420426066924-240-0.png"
+          alt="Camiseta"
+        />
+        <strong>Camiseta Geek</strong>
+        <span>R$ 80,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://d26lpennugtm8s.cloudfront.net/stores/726/919/products/camiseta-preta-st-center-ec76e86b0df341498315420426066924-240-0.png"
+          alt="Camiseta"
+        />
+        <strong>Camiseta Geek</strong>
+        <span>R$ 80,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://d26lpennugtm8s.cloudfront.net/stores/726/919/products/camiseta-preta-st-center-ec76e86b0df341498315420426066924-240-0.png"
+          alt="Camiseta"
+        />
+        <strong>Camiseta Geek</strong>
+        <span>R$ 80,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://d26lpennugtm8s.cloudfront.net/stores/726/919/products/camiseta-preta-st-center-ec76e86b0df341498315420426066924-240-0.png"
+          alt="Camiseta"
+        />
+        <strong>Camiseta Geek</strong>
+        <span>R$ 80,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
